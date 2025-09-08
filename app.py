dba2183172957c8c47c8bc4fccadf548e36d5463
CrescentUniversity1 <dfabulousboy12@gmail.com>
2025-09-08 18:55:49 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,13 @@ import os
 import streamlit as st
 
 from utils.rag_pipeline import RAGIndex, Generator, RAGPipeline, ingest_json_files
+from utils.embedding import load_model, load_dataset, compute_question_embeddings
+from utils.preprocess import preprocess_text
+from utils.search import find_response
+from utils.memory import init_memory
+from utils.log_utils import log_query
+from utils.greetings import is_greeting, greeting_responses, is_social_trigger, social_response
+from utils.course_query import extract_course_query  # for extracting level/semester
 
 INDEX_DIR = "RAG-MODEL/index"
 DATA_DIR = "RAG-MODEL/data"
