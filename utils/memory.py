bdcfbd6c8dfa976d1786f02189c360e3d63f0646
CrescentUniversity1 <dfabulousboy12@gmail.com>
2025-09-11 19:52:27 +0100

    Update memory.py

--- utils/memory.py
+++ utils/memory.py
@@ -13,8 +13,8 @@ def init_memory():
             "keywords": [],
             "sentiment": None
         }
-    if "chat" not in st.session_state:
-        st.session_state["chat"] = []
+    if "messages" not in st.session_state:  # Changed from 'chat' to 'messages' to match app.py
+        st.session_state["messages"] = []
     if "bot_greeted" not in st.session_state:
         st.session_state["bot_greeted"] = False
 
